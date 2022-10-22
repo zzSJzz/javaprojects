c0de7e283da34a7d373a6443c9cfd4282bcd741f
Samuel <samueljc003@gmail.com>
2022-10-22 08:29:17 -0300

    git push

---
+++ atividadesDeClasse/segundaSemana/contaBancaria.java
@@ -0,0 +1,20 @@
+package javaprojects.atividadesDeClasse.segundaSemana;
+
+import java.util.Scanner;
+
+public class contaBancaria {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String user;
+        Int senha;
+
+
+
+
+
+
+        input.close();
+    }
+
+
+}
